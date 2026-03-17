3755ce0258c07d6b9bd4b1bf01d4144ec7bf8643
Roberta Parcianello <roberta.parcianello@ifpr.edu.br>
2026-03-17 11:07:09 -0300

    Primeiro commit

---
+++ dao/src/main/java/br/edu/ifpr/dao/generics/DAOLista.java
@@ -0,0 +1,35 @@
+package br.edu.ifpr.dao.generics;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DAOLista<T> implements GenericDAO<T> {
+    private List<T> lista;
+
+    public DAOLista() {
+        this.lista = new ArrayList<>();
+    }
+
+    @Override
+    public void inserir(T object) {
+       lista.add(object);
+    }
+
+    @Override
+    public List<T> listar() {
+       return lista;
+    }
+
+    @Override
+    public T buscarPorId(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'buscarPorId'");
+    }
+
+    @Override
+    public void remover(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remover'");
+    }
+
+}
